ae9a8e7bc0039c909317b96a2112e22150962041
Samantha <samanthavillafana8@gmail.com>
2024-05-28 17:46:26 -0400

    create route.js with imports

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import Records from "./Records";
+import RecordForm from "./RecordForm";
+
+
+const routes = [
+    {
+        path:"/",
+        element: <Home />,
+    }
+]

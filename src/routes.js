da34a68be5a24513d643540a81a0ddbd5305a399
Samantha <samanthavillafana8@gmail.com>
2024-05-28 17:48:08 -0400

    route setup

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,15 @@ const routes = [
     {
         path:"/",
         element: <Home />,
+    },
+    {
+        path:"/records",
+        element: <Records />
+    },
+    {
+        path: "/newrecordform",
+        element: <RecordForm />
     }
 ]
+
+export default routes

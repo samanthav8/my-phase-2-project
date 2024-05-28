f1dc652196436b82fa664363a21e8b90c5aee155
Samantha <samanthavillafana8@gmail.com>
2024-05-28 17:57:16 -0400

    reorganizing components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import routes from "../routes.js"
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;

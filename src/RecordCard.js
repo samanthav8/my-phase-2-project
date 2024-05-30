83156c9993ecd6d7eb6be1bb30cd58523d5c35f5
Samantha <samanthavillafana8@gmail.com>
2024-05-30 15:07:43 -0400

    change name to title and setting state for form

--- src/RecordCard.js
+++ src/RecordCard.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const RecordCard = ({ record }) => {
   return (
     <div>
-      <img src={record.cover} alt={record.name} />
-      <h1>{record.name}</h1>
+      <img src={record.cover} alt={record.title} />
+      <h1>{record.title}</h1>
       <p>{record.artist}</p>
       <p>{record.genre}</p>
     </div>
--- src/RecordForm.js
+++ src/RecordForm.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const RecordForm = () => {
+  const [title, setTitle] = useState('');
+  const [artist, setArtist] = useState('');
+  const [year, setYear] = useState('');
+  const [genre, setGenre] = useState('');
+  
   return (
     <div>
       <h1>NEW RECORD FORM</h1>

eff59e64ffc9932d212051452baef376da12059d
Samantha <samanthavillafana8@gmail.com>
2024-06-01 13:12:11 -0400

    set up usePaams

--- src/RecordCard.js
+++ src/RecordCard.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const RecordCard = ({ record }) => {
-  console.log(record)
+  console.log('RecordCard record:', record); // Add this for debugging
+
   return (
     <div>
-      <img src={record.cover} alt={record.title} />
-      <h1>{record.title}</h1>
+      <img src={record.cover} alt={record.name} />
+      <h1>{record.name}</h1>
       <p>{record.artist}</p>
       <p>{record.genre}</p>
+      <Link to={`/records/${record.id}`}>View Details</Link>
     </div>
   );
 };
--- src/RecordInfo.js
+++ src/RecordInfo.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useParams, useOutletContext } from 'react-router-dom';
 
 const RecordInfo = () => {
+  const { id } = useParams();
+  const records = useOutletContext();
+
+  const record = records.find(record => record.id === id); 
+
+  if (!record) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
-      
+      <h1>{record.title}</h1>
+      <p>Artist: {record.artist}</p>
+      <p>Genre: {record.genre}</p>
+      <img src={record.cover} alt={record.name} />
     </div>
-  )
-}
+  );
+};
 
-export default RecordInfo
+export default RecordInfo;
